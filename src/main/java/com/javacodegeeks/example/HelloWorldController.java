3cd7b94621ffa199ad10091364827cf03d88b22e
muthu <muthumuthu.m muthu.m@2HPI5044.CTL.local>
2020-01-07 18:49:58 +0530

    added another base url

--- src/main/java/com/javacodegeeks/example/HelloWorldController.java
+++ src/main/java/com/javacodegeeks/example/HelloWorldController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+	@GetMapping("/")
+	public String getMsg() {
+		return "Testing";
+	}
+	
 	@GetMapping("/hello")
 	public String sayHello() {
 		return "Hello Java Code Geeks!";
